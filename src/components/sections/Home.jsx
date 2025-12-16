3113e6a3f373e6db92d3e777262de62b8485d212
riz3ndrr <riz3ndrr@gmail.com>
2025-12-16 19:34:04 +1300

    Loading animation finished

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -7,23 +7,32 @@ export const Home = () => {
 
   useEffect(() => {
     const t1 = setTimeout(() => setAnimationStage(1), 1000);
-    const t2 = setTimeout(() => setAnimationStage(2), 2500);
-
+    const t2 = setTimeout(() => setAnimationStage(2), 2000);
+    const t3 = setTimeout(() => setAnimationStage(3), 3000);
+    const t4 = setTimeout(() => setAnimationStage(4), 4000);
+    const t5 = setTimeout(() => setAnimationStage(5), 5000);
   return () => {
       clearTimeout(t1)
       clearTimeout(t2)
+      clearTimeout(t3)
+      clearTimeout(t4)
+      clearTimeout(t5)
 
     }
   }, [])
   
   const animationStages = {
-    1 : {icon : PiCursor        , classes : "z-1 translate-x-[30vw] translate-y-10 animate-cursor-1"},
-    2 : {icon : FaRegHandPointer, classes : "z-1 -translate-x-105 translate-y-10 animate-cursor-2" },
-    3 : {icon : PiCursor        , classes : "z-1 -translate-x-20 translate-y-10"},
+    1 : {icon : PiCursor        , iconClasses : " -translate-x-100 translate-y-10 animate-cursor-1", titleAnimation : "", subtitleAnimation : "" },
+    2 : {icon : FaRegHandPointer, iconClasses : " -translate-x-105 translate-y-10 animate-cursor-2" , titleAnimation : "animate-resize", subtitleAnimation : ""},
+    3 : {icon : PiCursor        , iconClasses : "-translate-x-20 translate-y-10 animate-cursor-3", titleAnimation : "animate-resize", subtitleAnimation : ""},
+    4 : {icon : FaRegHandPointer, iconClasses : "-translate-x-30 translate-y-40 animate-cursor-1", titleAnimation : "animate-resize", subtitleAnimation : "animate-subtitle-resize"},
+    5 : {icon : PiCursor,         iconClasses : "-translate-x-30 translate-y-40 ", titleAnimation : "animate-resize", subtitleAnimation : "animate-subtitle-resize"},
   }
 
   const CurrentIcon = animationStages[stageNum].icon
-  const CurrentClasses = animationStages[stageNum].classes
+  const CurrentClasses = animationStages[stageNum].iconClasses
+  const TitleAnimation = animationStages[stageNum].titleAnimation
+  const SubtitleAnimation = animationStages[stageNum].subtitleAnimation
 
   return (
     <section className = "h-screen">
@@ -32,16 +41,16 @@ export const Home = () => {
         <div className = "flex flex-row items-end ml-35">
           <div className = "flex flex-row justify-start w-2xl ">
             <h1 className = "bg-[#55bd5c] py-4 px-10 rounded-xl text-8xl text-white font-mono font-bold">Liam</h1>
-            <h1 className = "bg-[#55bd5c] py-4 -ml-2 w-0 rounded-r-xl text-8xl overflow-hidden text-white font-mono font-bold animate-resize">Wadek</h1>
+            <h1 className = {` bg-[#55bd5c] py-4 -ml-2 w-0 rounded-r-xl text-8xl overflow-hidden text-white font-mono font-bold ${TitleAnimation}`}>Wadek</h1>
           </div>
 
-          <div>
-            <CurrentIcon size = {90} color = {"white"} className = {CurrentClasses}/>
+          <div className = {`z-1 ${CurrentClasses}`}>
+            <CurrentIcon size = {90} color = {"white"}/>
           </div>
 
           
         </div>
-          <h2 className = "bg-[#a655bd] py-4 px-5 text-6xl rounded-xl text-white font-mono font-bold">Film Freelancer</h2>
+          <h2 className = {`z-0 translate-x-[100vw] bg-[#a655bd] py-4 px-5 text-6xl rounded-xl text-white font-mono font-bold  ${SubtitleAnimation}`}>Film Freelancer</h2>
       </div> 
 
     </section>
