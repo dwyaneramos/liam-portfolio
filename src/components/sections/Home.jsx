45978fafc688b35d19256b4fa4724f4ea47ba21c
riz3ndrr <riz3ndrr@gmail.com>
2025-12-19 21:57:59 +1300

    made progress on loading animations - need to work on adjusting positioning of cursor and responsive design

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -41,7 +41,9 @@ export const ClipRow = ({fromRight}) => {
   )
 }
 
+
 export const Home = () => {
+  const [showEditor, setShowEditor] = useState(false);
   const [stageNum, setAnimationStage] = useState(1);
 
   useEffect(() => {
@@ -49,44 +51,141 @@ export const Home = () => {
     const t2 = setTimeout(() => setAnimationStage(2), 1500);
     const t3 = setTimeout(() => setAnimationStage(3), 3000);
     const t4 = setTimeout(() => setAnimationStage(4), 4000);
-    const t5 = setTimeout(() => setAnimationStage(5), 5000);
+    const t5 = setTimeout(() => {
+      setAnimationStage(5);
+      setShowEditor(true);
+    }, 5000);
+    const t6 = setTimeout(() => setAnimationStage(6), 6000);
+
+
   return () => {
       clearTimeout(t1)
       clearTimeout(t2)
       clearTimeout(t3)
       clearTimeout(t4)
       clearTimeout(t5)
+      clearTimeout(t6)
 
     }
   }, [])
+
+  return (
+    <div className="max-h-screen min-h-full overflow-y-hidden">
+      <Editor showEditor = {showEditor} stageNum = {stageNum}/>
+      <Cursor stageNum = {stageNum}/>
+      <Toolbar showEditor = {showEditor}/>
+      <Timeline setShowEditor = {setShowEditor} stageNum = {stageNum}/>
+      
+    </div>
+  )
+}
+
+const Editor = ({showEditor}) => {
+  useEffect(() => {
+    if (showEditor) {
+      console.log("YSAY");
+    }
+  }, [showEditor])
+
+  return (
+    <div className = {`overflow-hidden max-h-0 bg-red-400 ${showEditor ? "animate-display-editor" : ""}`}>
+      <EditorHeader/>
+      <FilesNavBar/>
+    </div>
+  )
+}
+
+const EditorHeader = () => {
+  return (
+    <div className = "bg-[#3b4347] w-screen py-3">
+      <h1 className = "font-mono text-white text-2xl text-center">Editing portfolio.mp4</h1>
+    </div>
+  )
+}
+
+const FilesNavBar = () => {
+  return (
+    <div className="bg-[#3b4347] w-xs">
+      
+      <h1 className ="pl-10 pt-10 font-mono text-white text-4xl">Media</h1>  
+      <div className = "p-10 flex flex-wrap gap-3 flex-col  min-h-screen">
+        <File header = "Home"/>  
+        <File header = "About"/>  
+        <File header = "Projects"/>  
+      </div>
+    </div>
+  )
+}
+
+const File = ({header}) => {
+  return (
+    <div className = "hover:-translate-y-1 transition h-30 w-50 bg-blue-400 flex items-end  ">
+      <h1 className = "mx-auto">{header}.mp4</h1>
+    </div>
+  )
+}
+
+const Toolbar = ({showEditor}) => {
+
+  const EditIconSize = 55;
+  return (
+    <div className = {`absolute top-0 bg-[#4f5b61] min-h-20 max-h-20 py-3 min-w-screen flex
+          flex-row gap-10 items-align items-center z-0
+          ${showEditor ? "animate-move-toolbar" : ""}`}>
+      <RiArrowGoBackFill size = {EditIconSize} color = {"white"} className="ml-10"/>
+      <RiArrowGoForwardFill size = {EditIconSize} color = {"white"}/>
+      <FaPlay size = {EditIconSize} color = {"white"}/>
+      <FaStop size = {EditIconSize} color = {"white"}/>
+      
+    </div>
+  )
+}
+
+
+const Cursor = ({stageNum}) => {
+
+  const animationStages = {
+    1 : {icon : PiCursor        , iconClasses : " -translate-x-100 translate-y-10 animate-cursor-1"},
+    2 : {icon : FaRegHandPointer, iconClasses : " -translate-x-105 translate-y-10 animate-cursor-2"},
+    3 : {icon : PiCursor        , iconClasses : "-translate-x-20 translate-y-10 animate-cursor-3" },
+    4 : {icon : FaRegHandPointer, iconClasses : "-translate-x-30 translate-y-40 animate-cursor-1"}  ,
+    5 : {icon : PiCursor,         iconClasses : "-translate-x-30 translate-y-40 animate-cursor-4"}, 
+    6 : {icon : PiCursor,         iconClasses : "-translate-x-30 translate-y-40 animate-cursor-5"}  
+  }
+
+  const CurrentIcon = animationStages[stageNum].icon
+  const CurrentClasses = animationStages[stageNum].iconClasses
+  
+  return (
+    <div className = {`z-10 absolute right-105 top-1/8 ${CurrentClasses}`}>
+      <CurrentIcon size = {90} color = {"white"}/>
+    </div>
+  )
+
+}
+
+const Timeline = ({setShowEditor, stageNum}) => {
   
   const animationStages = {
     1 : {icon : PiCursor        , iconClasses : " -translate-x-100 translate-y-10 animate-cursor-1", titleAnimation : "", subtitleAnimation : "" },
     2 : {icon : FaRegHandPointer, iconClasses : " -translate-x-105 translate-y-10 animate-cursor-2" , titleAnimation : "animate-resize", subtitleAnimation : ""},
     3 : {icon : PiCursor        , iconClasses : "-translate-x-20 translate-y-10 animate-cursor-3", titleAnimation : "animate-resize", subtitleAnimation : ""},
     4 : {icon : FaRegHandPointer, iconClasses : "-translate-x-30 translate-y-40 animate-cursor-1", titleAnimation : "animate-resize", subtitleAnimation : "animate-subtitle-resize"},
-    5 : {icon : PiCursor,         iconClasses : "-translate-x-30 translate-y-40 ", titleAnimation : "animate-resize", subtitleAnimation : "animate-subtitle-resize"},
+    5 : {icon : PiCursor,         iconClasses : "-translate-x-30 translate-y-40 animate-cursor-4", titleAnimation : "animate-resize", subtitleAnimation : "animate-subtitle-resize"},
+    6 : {icon : PiCursor,         iconClasses : "-translate-x-30 translate-y-40 animate-cursor-4", titleAnimation : "animate-resize", subtitleAnimation : "animate-subtitle-resize"},
   }
 
-  const CurrentIcon = animationStages[stageNum].icon
-  const CurrentClasses = animationStages[stageNum].iconClasses
+
   const TitleAnimation = animationStages[stageNum].titleAnimation
   const SubtitleAnimation = animationStages[stageNum].subtitleAnimation
 
   return (
-    <section className = "h-screen">
-      <div className = "relative flex h-screen flex-col items-align items-center justify-center gap-0 ">
+    <section className = "h-screen overflow-hidden mt-20">
+      <div className = "relative flex h-screen flex-col   justify-start gap-0 ">
 
-          <div className = "bg-[#4f5b61] min-h-30 min-w-screen flex flex-row gap-10 items-align items-center">
-            <RiArrowGoBackFill size = {90} color = {"white"} className="ml-10"/>
-            <RiArrowGoForwardFill size = {90} color = {"white"}/>
-            <FaPlay size = {90} color = {"white"}/>
-            <FaStop size = {90} color = {"white"}/>
-            
-          </div>
           
           {/*Name Clip*/}
-          <div className = "pl-40 bg-[#243036] min-w-screen py-5 min-h-40 flex flex-row items-center justify-center">
+          <div className = "pl-40 py-5 min-h-40 flex flex-row items-center justify-center">
             <div className = "flex flex-col">
               <div className = "flex flex-row">
                 <h2 className = {`text-3xl pl-2 pt-2 font-mono text-white  bg-[#4fab55] rounded-t-xl w-xs`}>
@@ -100,17 +199,14 @@ export const Home = () => {
                 <h1 className = {` bg-[#55bd5c] py-4 -ml-3 w-0 rounded-br-xl text-8xl overflow-hidden text-white font-mono font-bold ${TitleAnimation}`}>Wadek</h1>
               </div>
             </div>
-
-            <div className = {`z-1 ${CurrentClasses}`}>
-              <CurrentIcon size = {90} color = {"white"}/>
-            </div>
-
+          
+            
           
           </div>
 
         
           {/*Title Clip*/}
-          <div className = "pl-10 bg-[#243036] min-w-screen min-h-30 pb-5 flex flex-row items-center justify-center">
+          <div className = "pl-10 min-h-30 pb-5 flex flex-row items-center justify-center">
             <div className = "flex flex-col ">
               <h2 className ={`bg-[#844496] rounded-t-xl font-mono text-white text-2xl pl-2 pt-2 translate-x-[100vw] ${SubtitleAnimation}`}>What I am:</h2>
               <h2 className = {`z-0 translate-x-[100vw] bg-[#a655bd] py-4 px-5 text-6xl rounded-b-xl text-white font-mono font-bold ${SubtitleAnimation}`}>Film Freelancer</h2>
