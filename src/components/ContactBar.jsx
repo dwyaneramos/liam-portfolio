19c6d0390d59bac3a609d33289f2dc28b2e55c20
riz3ndrr <riz3ndrr@gmail.com>
2025-12-26 18:11:46 +1300

    tweaked contact bar and navbar

--- src/components/ContactBar.jsx
+++ src/components/ContactBar.jsx
@@ -10,8 +10,13 @@ export const ContactBar = () => {
     <div className = "absolute bottom-0 bg-[#4f5b61] min-h-20 max-h-20 py-3 min-w-screen flex
       flex-row gap-5 items-align items-center z-0">
 
-    <IoMailOutline size = {IconSize} color = {"white"} className="ml-10"/>
-    <FaLinkedin size = {IconSize} color = {"white"}/>
+    <a href="mailto:riz3ndrr@gmail.com" className ="hover:-translate-y-1 transition">
+      <IoMailOutline size = {IconSize} color = {"white"} className="ml-10"/>
+    </a>
+
+    <a href="" className ="hover:-translate-y-1 transition">
+      <FaLinkedin size = {IconSize} color = {"white"}/>
+    </a>
     </div> 
   )
 }
--- src/components/sections/Editor.jsx
+++ src/components/sections/Editor.jsx
@@ -10,6 +10,7 @@ import {Projects} from "./Projects"
 
 import {Homepage} from './Homepage'
 import {About} from './About'
+import {NavBar} from '../NavBar'
 
 import emailjs from '@emailjs/browser';
 
@@ -33,28 +34,6 @@ export const Editor = ({page, setPage}) => {
 }
 
 
-const NavBar = ({setPage, page}) => {
-  return (
-    <div className = "flex flex-row pl-10 py-2 gap-5 text-white font-mono text-2xl items-center">
-      <Option Icon = {FaHome} name ="Home" isBig = {true} onClick={() => setPage("Home")} current = {page == "Home" ? true : false}/>
-      <Option Icon = {IoPerson} name ="About Me" isBig = {true} onClick={() => setPage("About")} current = {page == "About" ? true : false}/>
-      <Option Icon = {FaFilm} name ="Projects" isBig = {true} onClick={() => setPage("Projects")} current = {page == "Projects" ? true : false}/>
-    </div>
-  )
-}
-
-const Option = ({Icon, name, isBig, onClick, current}) => {
-  const iconSize = isBig ? 29 : 23
-  const textSize = isBig ? "text-2xl" : "text-xl" 
-  const bg = current ? "bg-blue-500" : ""
-
-  return (
-    <div className = {`flex flex-row gap-2 hover:bg-gray-700 transition cursor-pointer p-2 rounded-lg ${bg}`} onClick = {onClick}>
-      <Icon size={iconSize} color={"white"}/>
-      <h1 className = {`${textSize} text-white`}>{name}</h1>
-    </div>
-  )
-}
 
 const VideoOption = ({Icon, name}) => {
   return (
